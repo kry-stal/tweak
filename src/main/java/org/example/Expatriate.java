700157ac4fd2bbf9c9b3190a9cbe2434d3794973
kry-stal <ceejaygrace57@gmail.com>
2023-07-02 23:29:44 +0100

    testing

--- src/main/java/org/example/Expatriate.java
+++ src/main/java/org/example/Expatriate.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Expatriate {
     public static void main(String[] args) {
-
+        System.out.println("hllo");
 
     }
 }
--- src/main/java/org/example/Midnight.java
+++ src/main/java/org/example/Midnight.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Midnight {
     public static void main(String[] args) {
-//        String str ="";
+        String str ="";
 //        StringBuffer StringBuffer = new StringBuffer();
 //        StringBuilder StringBuilder = new StringBuilder();
 //        String a = "Ada";
